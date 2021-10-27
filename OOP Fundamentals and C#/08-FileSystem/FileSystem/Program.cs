cb430faa2d8171b880706865bc78c5e9248f101d
Dan Patrascu-Baba <dan@danpatrascu.com>
2021-10-27 17:10:00 +0300

    Add basic filesystem demo

---
+++ OOP Fundamentals and C#/08-FileSystem/FileSystem/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+
+namespace FileSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DriveInfo[] driveInfo = DriveInfo.GetDrives();
+
+            foreach (var info in driveInfo)
+            {
+                Console.WriteLine($"{info.Name}, {info.IsReady}, {info.AvailableFreeSpace}");
+            }
+
+            var dirInfo = new DirectoryInfo(Directory.GetCurrentDirectory());
+
+            Console.WriteLine(dirInfo.Name);
+            var parent = dirInfo.Parent;
+            Console.WriteLine(parent.Name);
+
+            var files = dirInfo.GetFiles();
+
+            foreach (var item in files)
+            {
+                Console.WriteLine($"{item.Name}, {item.Extension}, {item.DirectoryName}");
+            }
+
+            var newDirPath = Path.Combine(Directory.GetCurrentDirectory(), "MyFolder");
+            Directory.CreateDirectory(newDirPath);
+
+            var newFile = Path.Combine(newDirPath, "example.txt");
+            File.Create(newFile);
+
+            var textFile = Path.Combine(newDirPath, "hello.txt");
+            var fileStream = File.Create(textFile);
+
+            var sw = new StreamWriter(fileStream);
+            for (int i = 1; i <= 10; i++)
+            {
+                sw.WriteLine($"This is line number: {i}");
+            }
+            sw.Close();
+
+            using (var sr = new StreamReader(textFile))
+            {
+                var read = true;
+
+                while (read)
+                {
+                    var line = sr.ReadLine();
+                    if (string.IsNullOrEmpty(line))
+                        read = false;
+                    else Console.WriteLine(line);
+                }
+            }
+            
+
+
+            var otherSr = new StreamReader(textFile);
+            Console.WriteLine(otherSr.ReadToEnd());
+
+            otherSr.Close();
+        }
+    }
+}
