723c0660f45856c686dbf395faa891732aa4ba45
Dan Patrascu-Baba <dan@danpatrascu.com>
2022-03-16 13:30:15 +0200

    Add sampel for template method pattern

---
+++ OOP Fundamentals and C#/10-BehavioralPatterns/TemplateMethod/DataMiner.cs
@@ -0,0 +1,32 @@
+﻿namespace TemplateMethod;
+
+public abstract class DataMiner
+{
+    public void MineData()
+    {
+        Console.WriteLine("Data mining process has started....");
+        OpenFile();
+        ReadData();
+        ParseData();
+        AnalyzeData();
+        SendReport();
+        CloseFile();
+        Console.WriteLine("Data mining process completed...");
+    }
+
+    protected abstract void OpenFile();
+    protected abstract void ReadData();
+    protected abstract void ParseData();
+
+    protected void AnalyzeData()
+    {
+        Console.WriteLine("Analyzing data....");
+    }
+
+    protected void SendReport()
+    {
+        Console.WriteLine("Sending report....");
+    }
+
+    protected abstract void CloseFile();
+}
