2fe6ec185bbccb80dae5cfbd3bc0220a1eb83a15
Dan Patrascu-Baba <dan@danpatrascu.com>
2022-07-11 15:14:28 +0300

    Add async and await sample

---
+++ OOP Fundamentals and C#/16. AsyncAwait/BreakfastParallel/Program.cs
@@ -0,0 +1,93 @@
+﻿using System.Diagnostics;
+
+//Cooking breakfast
+Console.WriteLine("Let's cook breakfast!");
+
+var sw = new Stopwatch();
+sw.Start();
+
+var coffeeTask = MakeCoffeeAsync();
+var panFryingTask = PanFrying();
+var toastTask = MakeToastAsync();
+
+await Task.WhenAll(coffeeTask, panFryingTask, toastTask);
+
+sw.Stop();
+double seconds = sw.ElapsedMilliseconds / 1000;
+
+Console.WriteLine("Breakfast is ready!");
+Console.WriteLine($"it took you {seconds} seconds to cook breakfast");
+
+static void AddCoffeeIngredients()
+{
+    Console.WriteLine("Adding ingredients to the coffee machine");
+    Console.WriteLine("All ingredients added.");
+}
+
+static async Task<Coffee> MakeCoffeeAsync()
+{
+    AddCoffeeIngredients();
+    Console.WriteLine("Starting coffee making machine....");
+    await Task.Delay(1000);
+    Console.WriteLine("Coffee is ready");
+    return new Coffee();
+}
+
+static async Task HeatPanAsync()
+{
+    Console.WriteLine("Heating the pan");
+    await Task.Delay(1000);
+    Console.WriteLine("The pan is hot");
+}
+
+static async Task<Eggs> FryEggsAsync()
+{
+    Console.WriteLine("Frying two eggs...");
+    await Task.Delay(5000);
+    Console.WriteLine("Eggs are ready");
+    return new Eggs();
+}
+
+static async Task<Bacon> FryBaconAsync()
+{
+    Console.WriteLine("Frying two slices of bacon");
+    await Task.Delay(8000);
+    Console.WriteLine("Bacon is ready");
+    return new Bacon();
+}
+
+static async Task<Toast> MakeToastAsync()
+{
+    Console.WriteLine("Toasting the bread...");
+    await Task.Delay(2000);
+    Console.WriteLine("Toast is ready");
+    var toast = new Toast();
+    AddButterToToast(toast);
+    AddJamToToast(toast);
+    return toast;
+}
+
+static void AddButterToToast(Toast toast)
+{
+    Console.WriteLine("Adding butter to the toast...");
+    Console.WriteLine("Butter added to the toast");
+}
+
+static void AddJamToToast(Toast toast)
+{
+    Console.WriteLine("Adding jam over the butter");
+    Console.WriteLine("Jam added on the toast");
+}
+
+static async Task PanFrying()
+{
+    await HeatPanAsync();
+    var fryingEgssTask = FryEggsAsync();
+    var fryingBaconTask = FryBaconAsync();
+    await Task.WhenAll(fryingEgssTask, fryingBaconTask);
+}
+
+internal class Coffee {}
+internal class Eggs {}
+internal class Bacon {}
+internal class Toast {}
---
+++ OOP Fundamentals and C#/16. AsyncAwait/BreakfastSynchronous/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Diagnostics;
+
+
+Console.WriteLine("Let's cook breakfast!");
+
+var sw = new Stopwatch();
+sw.Start();
+
+AddCoffeeIngredients();
+var coffee = MakeCoffee();
+HeatPan();
+var eggs = FryEggs();
+var bacon = FryBacon();
+var toast = MakeToast();
+AddButterToToast(toast);
+AddJamToToast(toast);
+
+sw.Stop();
+double seconds = sw.ElapsedMilliseconds / 1000;
+
+Console.WriteLine("Breakfast is ready!");
+Console.WriteLine($"it took you {seconds} seconds to cook breakfast");
+
+static void AddCoffeeIngredients()
+{
+    Console.WriteLine("Adding ingredients to the coffee machine");
+    Console.WriteLine("All ingredients added.");
+}
+
+static Coffee MakeCoffee()
+{
+    Console.WriteLine("Starting coffee making machine....");
+    Task.Delay(1000).Wait();
+    Console.WriteLine("Coffee is ready");
+    return new Coffee();
+}
+
+static void HeatPan()
+{
+    Console.WriteLine("Heating the pan");
+    Task.Delay(1000).Wait();
+    Console.WriteLine("The pan is hot");
+}
+
+static Eggs FryEggs()
+{
+    Console.WriteLine("Frying two eggs...");
+    Task.Delay(5000).Wait();
+    Console.WriteLine("Eggs are ready");
+    return new Eggs();
+}
+
+static Bacon FryBacon()
+{
+    Console.WriteLine("Frying two slices of bacon");
+    Task.Delay(8000).Wait();
+    Console.WriteLine("Bacon is ready");
+    return new Bacon();
+}
+
+static Toast MakeToast()
+{
+    Console.WriteLine("Toasting the bread...");
+    Task.Delay(2000).Wait();
+    Console.WriteLine("Toast is ready");
+    return new Toast();
+}
+
+static void AddButterToToast(Toast toast)
+{
+    Console.WriteLine("Adding butter to the toast...");
+    Console.WriteLine("Butter added to the toast");
+}
+
+static void AddJamToToast(Toast toast)
+{
+    Console.WriteLine("Adding jam over the butter");
+    Console.WriteLine("Jam added on the toast");
+}
+
+internal class Coffee {}
+internal class Eggs {}
+internal class Bacon {}
+internal class Toast {}
